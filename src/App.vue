c633eb863d29b9c1b9b1c33e72c8e13397638960
Isabel Campos <isacampos8a@gmail.com>
2025-02-12 18:14:14 -0600

    Finished AddTransaction Component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     import Header from './components/Header.vue';
     import Balance from './components/Balance.vue';
     import IncomeExpenses from './components/IncomeExpenses.vue';
+    import AddTransaction from './components/Addtransaction.vue';
     import {ref, computed} from 'vue'
 
     const transactions = ref([
@@ -44,6 +45,7 @@
     <div class="container">
         <Balance :total="sum"></Balance>
         <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
+        <AddTransaction></AddTransaction>
     </div>
 
 
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <h3>Add a new Transaction</h3>
+    <form id="form" @submit.prevent="onSubmit">
+        <div class="form-control">
+            <label for="text">Enter Transaction</label>
+            <input type="text" id="text" v-model="text" placeholder="Enter Transaction...">
+        </div>
+        <div class="form-control">
+            <label for="amount">Enter Transaction</label>
+            <input type="text" id="text" v-model="amount" placeholder="Enter Negative Value for Expenses">
+        </div>
+
+
+    </form>
+
+</template>
